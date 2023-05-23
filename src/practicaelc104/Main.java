cbf6dc6ccd776673b3302fddce3d0762c32c1eba
MariaPaolaVargasFernandez <vargasmariapaola388@gmail.com>
2023-05-23 12:31:25 -0600

    practico 1

---
+++ src/practicaelc104/Main.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package practicaelc104;
+
+/**
+ *
+ * @author HP
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Imprimir "Hola Git+Markdown" en la consola.
+        System.out.println("Hola Git+Markdown");
+    }
+
+}
